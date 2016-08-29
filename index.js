15ac3d082149bb435ab076d6d9fe5ffe88a08262
Jem Young <jemuel.young@gmail.com>
2016-08-29 01:19:25 -0700

    Sync heart

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
+'use strict';
 // TOOD:
 // - html page
 // -
-// // WEB BLIETOOTH
+// // WEB BLUETOOTH
 
+const HEART_EL = window.document.querySelector('.heart');
 
 function activateBlue() {
-navigator.bluetooth.requestDevice({ filters: [{ services: ['heart_rate'] }] })
-.then(device => { console.log(device); })
-.catch(error => { console.log(error); });
+  navigator.bluetooth.requestDevice({
+      filters: [{
+        services: ['heart_rate']
+      }]
+    })
+    .then(device => {
+      console.log(device);
+    })
+    .then(service => service.getCharacteristic('heart_rate_measurement'))
+    .catch(error => {
+      console.log(`error`);
+    });
 }
+
+function pulseHeart(rate) {
+  setInterval(()=> {
+      navigator.vibrate(100);
+      console.log('vibrate');
+  }, 1000000/rate);
+  const foo = `heartscale ${1000/rate}s infinite`;
+  HEART_EL.style.animation = foo ;
+}
+
+
+const MINUTE = 60;
+const heartRate = 60;
+const computedHeart = (heartRate / MINUTE) * 1000;
+pulseHeart(computedHeart);
+
+// setInterval(()=>{pulseHeart(computedHeart);},10000);
